8e5fef63fcd45b300d17aff6152cad078ec05b10
Egor Galkin <egalkin@devexperts.com>
2021-03-23 17:48:01 +0300

    Add web client for service testing

---
+++ lab1/src/main/java/com/itmo/egalkin/generated/Device.java
@@ -0,0 +1,144 @@
+
+package com.itmo.egalkin.generated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for device complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="device">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="available" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="price" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *         &lt;element name="releaseYear" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="type" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "device", propOrder = {
+    "available",
+    "name",
+    "price",
+    "releaseYear",
+    "type"
+})
+public class Device {
+
+    protected boolean available;
+    protected String name;
+    protected double price;
+    protected int releaseYear;
+    protected String type;
+
+    /**
+     * Gets the value of the available property.
+     * 
+     */
+    public boolean isAvailable() {
+        return available;
+    }
+
+    /**
+     * Sets the value of the available property.
+     * 
+     */
+    public void setAvailable(boolean value) {
+        this.available = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the price property.
+     * 
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * Sets the value of the price property.
+     * 
+     */
+    public void setPrice(double value) {
+        this.price = value;
+    }
+
+    /**
+     * Gets the value of the releaseYear property.
+     * 
+     */
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    /**
+     * Sets the value of the releaseYear property.
+     * 
+     */
+    public void setReleaseYear(int value) {
+        this.releaseYear = value;
+    }
+
+    /**
+     * Gets the value of the type property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the value of the type property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setType(String value) {
+        this.type = value;
+    }
+
+}
