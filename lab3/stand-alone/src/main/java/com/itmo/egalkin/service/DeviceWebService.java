82ca5f530ea3fff2d8be44c112e0fdb97113f12c
Egor Galkin <egalkin@devexperts.com>
2021-06-06 14:42:47 +0300

    Add binary data uploading to service

--- lab3/stand-alone/src/main/java/com/itmo/egalkin/service/DeviceWebService.java
+++ lab3/stand-alone/src/main/java/com/itmo/egalkin/service/DeviceWebService.java
@@ -12,10 +12,15 @@ import com.itmo.egalkin.exception.NegativePriceException;
 import com.itmo.egalkin.model.Device;
 import com.itmo.egalkin.model.PostgreSQLDAO;
 
+import java.awt.Image;
 import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlMimeType;
+import javax.xml.ws.WebServiceException;
+import javax.xml.ws.soap.MTOM;
 
+@MTOM
 @WebService(serviceName = "DeviceService")
 public class DeviceWebService {
 
@@ -60,6 +65,14 @@ public class DeviceWebService {
     public String deleteDevice(long id) throws DeviceNotFoundException {
         PostgreSQLDAO dao = new PostgreSQLDAO();
         return dao.deleteDevice(id);
+    }
+
+    @WebMethod(operationName = "uploadDeviceImageMock")
+    public String uploadDeviceImageMock(@XmlMimeType("image/*")Image data) {
+        if (data != null) {
+            return "Device image uploaded successfully";
+        }
+        throw new WebServiceException("Upload failed");
     }
 
 }
